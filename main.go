38f86e5f485e20943cd1e07e410d9434a7c565b5
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-12-12 09:39:47 +0700

    update

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"golang-api4/controllers"
+	"golang-api4/database"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func main() {
+	LoadAppConfig()
+
+	database.Connect(AppConfig.ConnectionString)
+	database.Migrate()
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	RegisterProductRoutes(router)
+
+	log.Println(fmt.Sprintf("Starting server on port %s", AppConfig.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", AppConfig.Port), router))
+}
+
+func RegisterProductRoutes(router *mux.Router) {
+	router.HandleFunc("/api/products", controllers.GetProducts).Methods("GET")
+	router.HandleFunc("/api/products/{id}", controllers.GetProductById).Methods("GET")
+	router.HandleFunc("/api/products", controllers.CreateProduct).Methods("POST")
+	router.HandleFunc("/api/products/{id}", controllers.UpdateProduct).Methods("PUT")
+	router.HandleFunc("/api/products/{id}", controllers.DeleteProduct).Methods("DELETE")
 }
